86490c3c3c3293b8d275aeca9dfc5d45cea66bf1
Oleg Dreyman <dreymonde@me.com>
2016-03-26 14:49:42 +0200

    Dependencies added

---
+++ Package.swift
@@ -0,0 +1,11 @@
+import PackageDescription
+
+let package = Package(
+    name: "QubularServer",
+    dependencies: [
+    	.Package(url: "https://github.com/QuStudio/VocabulaireLinux.git", majorVersion: 0, minor: 1),
+    	.Package(url: "https://github.com/Zewo/HTTPServer.git", majorVersion: 0, minor: 3),
+    	.Package(url: "https://github.com/Zewo/Router.git", majorVersion: 0, minor: 3),
+    	.Package(url: "https://github.com/Zewo/LogMiddleware.git", majorVersion: 0, minor: 3)
+    ]
+)
