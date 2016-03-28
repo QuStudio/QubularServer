3869fa8398130bc894965d8a1bea894c1a64a9da
Oleg Dreyman <dreymonde@me.com>
2016-03-28 16:46:32 +0300

    Server is now working and even can recieve Users o_O

--- Package.swift
+++ Package.swift
@@ -6,6 +6,8 @@ let package = Package(
     	.Package(url: "https://github.com/QuStudio/VocabulaireLinux.git", majorVersion: 0, minor: 1),
     	.Package(url: "https://github.com/Zewo/HTTPServer.git", majorVersion: 0, minor: 3),
     	.Package(url: "https://github.com/Zewo/Router.git", majorVersion: 0, minor: 3),
-    	.Package(url: "https://github.com/Zewo/LogMiddleware.git", majorVersion: 0, minor: 3)
+    	.Package(url: "https://github.com/Zewo/LogMiddleware.git", majorVersion: 0, minor: 3),
+    	.Package(url: "https://github.com/Zewo/ContentNegotiationMiddleware.git", majorVersion: 0, minor: 3),
+    	.Package(url: "https://github.com/Zewo/JSONMediaType.git", majorVersion: 0, minor: 3)
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,40 @@
-print("Hello, world!")
+import HTTPServer
+import Router
+import LogMiddleware
+import ContentNegotiationMiddleware
+import JSONMediaType
+import VocabulaireLinux
+// import Topo
+
+let log = Log()
+let logger = LogMiddleware(log: log)
+
+let contentNegotiator = ContentNegotiationMiddleware(mediaTypes: JSONMediaType())
+
+let router = Router(middleware: contentNegotiator) { route in
+    route.get("/hello") { _ in
+        return Response(body: "hello world")
+    }
+    route.post("/user") { request in
+    	if let content = request.content, user = User.from(content) {
+    		print(user)
+    		return Response(body: String(content))
+    	}
+    	return Response(body: "Fuck off")
+    }
+    route.post("/debugUser") { request in
+    	if let content = request.content {
+    		do {
+    			let user = try User(map: Mapper(interchangeData: content))
+    			print(user.username)
+    			return Response(body: "yay")
+    		} catch {
+    			print(error)
+    			return Response(body: String(content))
+    		}
+    	}
+    	return Response(body: "Fuck off")
+    }
+}
+
+try Server(middleware: logger, responder: router).start()
