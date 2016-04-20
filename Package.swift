0cc1100076723538be5ccbc6e1910ad19641a0e5
Oleg Dreyman <dreymonde@me.com>
2016-04-20 22:43:25 +0300

    Bump to the latest snapshot

--- Package.swift
+++ Package.swift
@@ -3,11 +3,11 @@ import PackageDescription
 let package = Package(
     name: "QubularServer",
     dependencies: [
-    	.Package(url: "https://github.com/QuStudio/VocabulaireLinux.git", majorVersion: 0, minor: 1),
-    	.Package(url: "https://github.com/Zewo/HTTPServer.git", majorVersion: 0, minor: 3),
-    	.Package(url: "https://github.com/Zewo/Router.git", majorVersion: 0, minor: 3),
-    	.Package(url: "https://github.com/Zewo/LogMiddleware.git", majorVersion: 0, minor: 3),
-    	.Package(url: "https://github.com/Zewo/ContentNegotiationMiddleware.git", majorVersion: 0, minor: 3),
-    	.Package(url: "https://github.com/Zewo/JSONMediaType.git", majorVersion: 0, minor: 3)
+    	.Package(url: "https://github.com/QuStudio/VocabulaireLib.git", majorVersion: 0, minor: 2),
+    	.Package(url: "https://github.com/Zewo/HTTPServer.git", majorVersion: 0, minor: 5),
+    	.Package(url: "https://github.com/Zewo/Router.git", majorVersion: 0, minor: 5),
+    	.Package(url: "https://github.com/Zewo/LogMiddleware.git", majorVersion: 0, minor: 5),
+    	.Package(url: "https://github.com/Zewo/ContentNegotiationMiddleware.git", majorVersion: 0, minor: 5),
+    	.Package(url: "https://github.com/Zewo/JSONMediaType.git", majorVersion: 0, minor: 5),
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -3,35 +3,50 @@ import Router
 import LogMiddleware
 import ContentNegotiationMiddleware
 import JSONMediaType
-import VocabulaireLinux
-// import Topo
+import VocabulaireLib
 
-let log = Log()
-let logger = LogMiddleware(log: log)
+let logger = Logger(name: "Main", appender: StandartOutputAppender)
 
-let vocabulary: Vocabulary = []
+var vocabulary: Vocabulary = []
 
-let contentNegotiator = ContentNegotiationMiddleware(mediaTypes: JSONMediaType())
+let contentNegotiator = ContentNegotiationMiddleware(mediaTypes: [JSONMediaType()])
 
 let router = Router(middleware: contentNegotiator) { route in
     route.get("/hello") { _ in
         return Response(body: "hello world")
     }
     route.post("/user") { request in
-    	if let content = request.content, user = User.from(content) {
+    	if let content = request.content, user = User.makeWith(structuredData: content) {
     		print(user)
     		return Response(body: String(content))
     	}
     	return Response(body: "Fuck off")
     }
     route.post("/entry") { request in
-    	if let content = request.content, entry = Entry.from(content) {
+    	if let content = request.contednt, entry = Entry.makeWith(structuredData: content) {
     		print(entry)
     		vocabulary.append(entry)
     		return Response(body: String(content))
     	}
     	return Response(body: "Fuck off")
     }
+    route.post("/debugEntry") { request in
+        guard let content = request.content else {
+            return Response(body: "No content")
+        }
+        let map = Mapper(structuredData: content)
+        let entry = try! Entry(mapper: map)
+        print(entry)
+        vocabulary.append(entry)
+        return Response(body: String(entry))
+    }
+    route.post("/morpheme") { request in
+        guard let content = request.content, morpheme = Morpheme.makeWith(structuredData: content) else {
+            return Response(body: "No")
+        }
+        print(morpheme)
+        return Response(body: "Yes")
+    }
 }
 
-try Server(middleware: logger, responder: router).start()
+try Server(responder: router).start()
